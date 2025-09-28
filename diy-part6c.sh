fe1a7446c24f4e730c282c4fff566e8876140677
yizhanghong <865362414@qq.com>
2025-09-28 14:45:33 +0800

    Update diy-part6c.sh

--- diy-part6c.sh
+++ diy-part6c.sh
@@ -36,6 +36,13 @@ git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
 $GITHUB_WORKSPACE/preset-clash-core.sh
 
+# passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/passwall2 
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone --depth=1 https://github.com/gSpotx2f/luci-app-cpu-status.git package/luci-app-cpu-status
+git clone --depth=1 https://github.com/gSpotx2f/luci-app-temp-status.git package/luci-app-temp-status
+
 echo "
 # mosdns
 #CONFIG_PACKAGE_luci-app-mosdns=y
@@ -52,7 +59,49 @@ echo "
 # 易有云
 #CONFIG_PACKAGE_luci-app-linkease=y
 
+
+CONFIG_PACKAGE_luci-app-statistics=y
+CONFIG_PACKAGE_luci-app-temp-status=y
+CONFIG_PACKAGE_luci-app-cpu-status=y 
+CONFIG_PACKAGE_luci-app-passwall=y
+
+#
+# Configuration
+#
+# CONFIG_PACKAGE_luci-app-passwall_Iptables_Transparent_Proxy is not set
+CONFIG_PACKAGE_luci-app-passwall_Nftables_Transparent_Proxy=y
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Haproxy is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Hysteria is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_NaiveProxy is not set
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Client=y
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Libev_Server is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Rust_Client is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Shadowsocks_Rust_Server is not set
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Client=y
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_ShadowsocksR_Libev_Server is not set
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Simple_Obfs=y
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_SingBox is not set
+CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Trojan_Plus=y
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_tuic_client is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Geodata is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_V2ray_Plugin is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray is not set
+# CONFIG_PACKAGE_luci-app-passwall_INCLUDE_Xray_Plugin is not set
 " >> .config
+
+
+
+#升级 golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# iStoreOS 官方插件库
+git clone --depth=1 -b main https://github.com/linkease/istore-packages package/istore-packages
+git clone --depth=1 -b dev https://github.com/jjm2473/luci-app-diskman package/diskman
+git clone --depth=1 -b dev4 https://github.com/jjm2473/OpenAppFilter package/oaf
+git clone --depth=1 -b master https://github.com/linkease/nas-packages package/nas-packages
+git clone --depth=1 -b main https://github.com/linkease/nas-packages-luci package/nas-packages-luci
+git clone --depth=1 -b main https://github.com/jjm2473/openwrt-apps package/openwrt-apps
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.21.1/g' package/base-files/files/bin/config_generate
